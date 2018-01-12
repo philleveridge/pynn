874197cfe371c738336aa4d5faa174bd9a86c3de
philleveridge <phil.leveridge@gmail.com>
2018-01-12 16:22:51 +0000

    readme update

--- nn_test1.py
+++ nn_test1.py
@@ -10,28 +10,9 @@ n=Network([	Layer(3,4, "1"),
 n.verbose = True
 n.epoc_print = 10000
 
-"""
 n.train(60000, 
 	np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1] ]), 
 	np.array([[0,1,1,0]]).T)
-
-print("Evaluating:")
-print("  0 0 1 = " + n.eval(np.array([0,0,1]), 0))
-print("  0 1 1 = " + n.eval(np.array([0,1,1]), 1))
-print("  1 0 1 = " + n.eval(np.array([1,0,1]), 1))
-print("  1 1 1 = " + n.eval(np.array([1,1,1]), 0))
-"""
-
-print "Test 1 ========================================================================"
-np.random.seed(0)
-n=Network([	Layer(3,4, "1"), 
-		Layer(4,1, "2")])
-n.verbose = True
-n.epoc_print = 10000
-#n.cost_fn=CostFunctions.binary_cross_entropy_cost
-n.train_batch(60000, 
-	np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1] ]), 
-	np.array([[0,1,1,0]]).T, batch_size=2)
 
 print("Evaluating:")
 print("  0 0 1 = " + n.eval(np.array([0,0,1]), 0))
